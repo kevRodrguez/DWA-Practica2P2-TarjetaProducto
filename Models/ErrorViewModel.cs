7af324242af1e82180d5d90d2e361203fd38f583
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2026-03-17 12:07:48 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practica2P2_KevinRodriguez.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
